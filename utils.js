84ce331676d21eb8e9bb26b827364e35eea57fc8
maciabe <maciabe@hotmail.com>
2018-07-05 02:08:21 +0200

    start

--- utils.js
+++ utils.js
@@ -1,3 +1,9 @@
 
 $('div').addClass('red')
 $('li').hide().fadeIn(1500)
+
+$('li:first-child').on('click', function(){
+  $(this).remove()
+})
+
+$('span').css('color','yellow').css('font-size','3rem')
