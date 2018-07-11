911b271534895568567cac2d26eeaa8c48627c8f
maciabe <maciabe@hotmail.com>
2018-07-11 20:36:43 +0200

    timer

--- game.js
+++ game.js
@@ -2,11 +2,21 @@
 var empty = 'td:not(.red):not(.green)'
 var scoreDisplay = document.getElementById('score')
 var enemyDisplay = document.getElementById('enemy')
+var timeDisplay = document.getElementById('timer')
 var score = 0
 var enemy = 0
+var time = 10
 
 scoreDisplay.innerText= 'your score :  ' + score
 enemyDisplay.innerText= 'enemys score :  ' + enemy
+timeDisplay.innerText= timer + ' sec'
+
+
+// counting down time
+var timeInterval = setInterval(function(){
+  time-=1
+  time === 0 ? clearInterval(timeInterval) : timeDisplay.innerText= time + ' sec'
+},1000)
 
 
 // toggling td color
@@ -31,7 +41,7 @@ var intervalId = setInterval(function(){
 },400)
 
 
-// utility function
+// utility toggle function
 const toggleColors = (item, className) => {
   className = className || 'red'
   item.classList.contains(className)
